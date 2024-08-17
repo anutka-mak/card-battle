242ba4357dc0b65009d1aa467ece0fdbc8b27e8b
anutka-mak <annmakiichuk@gmail.com>
2024-08-17 09:58:04 +0300

    refactor: change method

--- src/controllers/deck-controller.js
+++ src/controllers/deck-controller.js
@@ -34,15 +34,15 @@ class DeckController {
         });
     }
 
-    static shuffle() {
-        this.cards.sort(() => Math.random() - 0.5);
-    }
-
     static drawCards(number) {
         const dealCards = this.cards.splice(0, number);
         this.renderCardCount();
 
         return dealCards;
+    }
+
+    static shuffle() {
+        this.cards.sort(() => Math.random() - 0.5);
     }
 
     static getCardsCount() {
@@ -56,8 +56,3 @@ class DeckController {
 }
 
 export default DeckController;
-
-/*у мене є такий код тепер треба реалізувати таким чином 
-що я коли я клікаю на карту і на поле то (атакер) то карточка просто додається але коли я defender то я обираю в 
-себе карту і потім треба щоб клік був не на поле а на блок pair 
-card щоб при можливості що там дві карти то я могла побити будь яку ну типу я сама оберу яку побити*/
